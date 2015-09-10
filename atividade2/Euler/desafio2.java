8485546bda99c3309cebb61b505265e166e4c881
Euler Santana <euler.santana@yahoo.com>
2015-09-09 23:07:31 -0300

    Nova versao  do desafio 2 da atividade 1

--- atividade2/Euler/desafio2.java
+++ atividade2/Euler/desafio2.java
@@ -1,8 +1,15 @@
 class  Desafio2 {
-
-	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
+	
+	Desafio2(){
 	}
 
-
+	public int  problema1(int valor){
+		return valor += 1;
+	}
+	
+	public static void main(String[] args) {
+		Desafio2 desafio2 = new Desafio2();
+		System.out.println(desafio2.problema1(19));
+		System.out.println("Olá mundo!");
+	}
 }
